79804e538e78908cec5f31befb4701793ac654ad
moonbear <ww516079@gmail.com>
2023-04-07 16:46:12 +0900

    PUT 방식의 이해와 실습

---
+++ demo2/src/main/java/com/example/demo2/model/PostRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.demo2.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class PostRequestDto {
+	
+	private String name;
+	private String age;
+	
+} // end of class
